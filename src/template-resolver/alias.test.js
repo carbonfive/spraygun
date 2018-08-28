6ce9ddc174b0f5ea7bd13f272d67c39de57daa21
Matt Brictson <mattb@carbonfive.com>
2018-08-28 12:15:22 -0700

    Set up CircleCI and add a simple test

---
+++ src/template-resolver/alias.test.js
@@ -0,0 +1,33 @@
+const resolve = require("./alias");
+
+jest.mock("./git", () => {
+  return jest.fn();
+});
+
+describe("resolve", () => {
+  describe("for a recognized alias", () => {
+    it("delegates to the git resolver", () => {
+      const gitResolve = require("./git");
+      gitResolve.mockReturnValue("/path/to/resolved/repo");
+
+      const path = resolve("react");
+
+      expect(path).toEqual("/path/to/resolved/repo");
+      expect(gitResolve).toHaveBeenCalledWith(
+        "git@github.com:carbonfive/spraygun-react.git"
+      );
+    });
+  });
+
+  describe("for an unrecognized alias", () => {
+    it("returns undefined", () => {
+      expect(resolve("foo")).toBeUndefined();
+    });
+  });
+});
+
+describe("aliases", () => {
+  it("exports an array of alias names", () => {
+    expect(resolve.aliases).toEqual(["express", "react"]);
+  });
+});
